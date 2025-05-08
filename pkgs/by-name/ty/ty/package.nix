12bced6d5adc2ee706ed6f337e8da9f81582fc1c
Benjamin Sparks <b.sparks@alugha.com>
2025-05-08 17:10:44 +0200

    ty: add passthru for its Python package

--- pkgs/by-name/ty/ty/package.nix
+++ pkgs/by-name/ty/ty/package.nix
@@ -9,6 +9,7 @@
 
   buildPackages,
   versionCheckHook,
+  python3Packages,
   nix-update-script,
 }:
 
@@ -74,6 +75,7 @@ rustPlatform.buildRustPackage (finalAttrs: {
   );
 
   passthru = {
+    tests.ty-python = python3Packages.ty;
     updateScript = nix-update-script { extraArgs = [ "--version=unstable" ]; };
   };
 
