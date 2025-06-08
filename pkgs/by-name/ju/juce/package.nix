97f99b7a366daa42ac9801c510c8eb01b1e0fb6d
Benjamin Sparks <b.sparks@alugha.com>
2025-06-08 20:42:00 +0200

    juce: remove versionCheckProgram

--- pkgs/by-name/ju/juce/package.nix
+++ pkgs/by-name/ju/juce/package.nix
@@ -85,7 +85,6 @@ stdenv.mkDerivation (finalAttrs: {
   nativeInstallCheckInputs = [
     versionCheckHook
   ];
-  versionCheckProgram = "${placeholder "out"}/bin/juceaide";
   versionCheckProgramArg = "version";
   doInstallCheck = true;
 
