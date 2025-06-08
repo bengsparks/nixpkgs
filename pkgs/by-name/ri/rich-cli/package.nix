71250dca2b54efae43b771f595ad7b1d037ec0bf
Benjamin Sparks <b.sparks@alugha.com>
2025-06-08 21:15:40 +0200

    rich-cli: remove versionCheckProgram

--- pkgs/by-name/ri/rich-cli/package.nix
+++ pkgs/by-name/ri/rich-cli/package.nix
@@ -55,7 +55,6 @@ python3Packages.buildPythonApplication rec {
   nativeCheckInputs = [
     versionCheckHook
   ];
-  versionCheckProgram = "${placeholder "out"}/bin/rich";
   versionCheckProgramArg = "--version";
 
   passthru = {
