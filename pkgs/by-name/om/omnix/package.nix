a20ff7dbe37b93f5444bf4ee3c040978ba2a92f7
Benjamin Sparks <b.sparks@alugha.com>
2025-06-08 21:01:56 +0200

    omnix: remove versionCheckProgram

--- pkgs/by-name/om/omnix/package.nix
+++ pkgs/by-name/om/omnix/package.nix
@@ -114,7 +114,6 @@ rustPlatform.buildRustPackage (finalAttrs: {
   nativeInstallCheckInputs = [
     versionCheckHook
   ];
-  versionCheckProgram = "${placeholder "out"}/bin/om";
   versionCheckProgramArg = "--version";
   doInstallCheck = true;
 
