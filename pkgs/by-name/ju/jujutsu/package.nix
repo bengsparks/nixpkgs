a4d8279ef05a526287676065f218ecf076af00fd
Benjamin Sparks <b.sparks@alugha.com>
2025-06-08 20:42:38 +0200

    jujutsu: remove versionCheckProgram

--- pkgs/by-name/ju/jujutsu/package.nix
+++ pkgs/by-name/ju/jujutsu/package.nix
@@ -92,7 +92,6 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];
-  versionCheckProgram = "${placeholder "out"}/bin/jj";
   versionCheckProgramArg = "--version";
 
   passthru = {
