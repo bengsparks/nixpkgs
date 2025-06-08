6ecdb52749f62877bc29d74c1fa7299b934c03bb
Benjamin Sparks <b.sparks@alugha.com>
2025-06-08 20:13:25 +0200

    criticality-score: remove versionCheckProgram

--- pkgs/by-name/cr/criticality-score/package.nix
+++ pkgs/by-name/cr/criticality-score/package.nix
@@ -39,8 +39,6 @@ buildGoModule rec {
   doInstallCheck = true;
 
   nativeInstallCheckInputs = [ versionCheckHook ];
-
-  versionCheckProgram = "${placeholder "out"}/bin/${meta.mainProgram}";
   versionCheckProgramArg = "--version";
 
   meta = {
