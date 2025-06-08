5ef122fe2a487f7491b3e35b288122d3ed673e0f
Benjamin Sparks <b.sparks@alugha.com>
2025-06-08 20:41:42 +0200

    jsonschema-cli: remove versionCheckProgram

--- pkgs/by-name/js/jsonschema-cli/package.nix
+++ pkgs/by-name/js/jsonschema-cli/package.nix
@@ -22,7 +22,6 @@ rustPlatform.buildRustPackage rec {
     versionCheckHook
   ];
   doInstallCheck = true;
-  versionCheckProgram = "${placeholder "out"}/bin/${meta.mainProgram}";
   versionCheckProgramArg = "--version";
 
   passthru.updateScript = nix-update-script { };
